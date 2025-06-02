01ff03956a816c458f8df9c8d07fab05da42cd36
Emilea Chin <emileachinn@gmail.com>
2025-06-02 00:50:55 +0000

    step 2 (mongoose)

--- myApp.js
+++ myApp.js
@@ -1,8 +1,19 @@
 require('dotenv').config();
 let mongoose = require('mongoose')
 
-let Person;
+const PersonSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  },
+  favoriteFoods: {
+    type: [String]
+  }
+})
 
+let Person = mongoose.model('Person', PersonSchema);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 

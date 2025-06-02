4ebd8de72fca17163f46895301783f0acf9568bb
Emilea Chin <emileachinn@gmail.com>
2025-06-02 04:42:46 +0000

    step 5 (mongoose)

--- myApp.js
+++ myApp.js
@@ -38,7 +38,10 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, (err, data) => {
+    if (err) throw err
+    done(null, data)
+  })
 };
 
 const findOneByFood = (food, done) => {

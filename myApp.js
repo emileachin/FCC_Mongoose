6e12ebf361f0473eec87d335dd843dcdddd4f3f4
Emilea Chin <emileachinn@gmail.com>
2025-06-03 06:08:05 +0000

    step 12 (mongoose)

--- myApp.js
+++ myApp.js
@@ -103,7 +103,11 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort({name: 1}).limit(2).select({name: true}).exec((err, data) => {
+    if (err) throw err;
+    done(null, data);
+  }
+  );
 };
 
 /** **Well Done !!**
